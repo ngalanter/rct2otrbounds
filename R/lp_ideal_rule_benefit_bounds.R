695ae6c6226fe76318f95af7ff9573f547177c95
Nina Galanter <ninagalanter@gmail.com>
2022-07-18 23:11:14 -0700

    Update lp_ideal_rule_benefit_bounds.R

--- R/lp_ideal_rule_benefit_bounds.R
+++ R/lp_ideal_rule_benefit_bounds.R
@@ -13,26 +13,27 @@
 transform_range <- function(y_range,
                             mean_worse,
                             mean_better,
-                            scale){
-
+                            scale) {
   #flip outcome scale if lower outcomes are better, transform range to start at 0
-  if(scale == "lower"){
-
-    new_y_range <- -1*y_range + max(y_range)
+  if (scale == "lower") {
+    new_y_range <- -1 * y_range + max(y_range)
     new_mean_worse <- max(y_range) - mean_worse
     new_mean_better <- max(y_range) - mean_better
 
   } else{
-
     new_y_range <- y_range - min(y_range)
     new_mean_worse <- mean_worse - min(y_range)
     new_mean_better <- mean_better - min(y_range)
 
   }
 
-  return(list(new_y_range = new_y_range,
-              new_mean_worse = new_mean_worse,
-              new_mean_better = new_mean_better))
+  return(
+    list(
+      new_y_range = new_y_range,
+      new_mean_worse = new_mean_worse,
+      new_mean_better = new_mean_better
+    )
+  )
 }
 
 #' Linear Programming Bound on the Ideal Rule Benefit
@@ -59,32 +60,32 @@ transform_range <- function(y_range,
 #'
 #' @export
 lp_ideal_rule_benefit <- function(m = NULL,
-                                          M = NULL,
-                                          y_range = NULL,
-                                              s2_1,
-                                              s2_0,
-                                              mean_1,
-                                              mean_0,
-                                              n_1=NULL,
-                                              n_0=NULL,
-                                              scale = "higher",
-                                              conf.upper = FALSE,
-                                              level = 0.95)
+                                  M = NULL,
+                                  y_range = NULL,
+                                  s2_1,
+                                  s2_0,
+                                  mean_1,
+                                  mean_0,
+                                  n_1 = NULL,
+                                  n_0 = NULL,
+                                  scale = "higher",
+                                  conf.upper = FALSE,
+                                  level = 0.95)
 {
-
-  if((is.null(m) | is.null(M)) & is.null(y_range)){
+  if ((is.null(m) | is.null(M)) & is.null(y_range)) {
     stop("Either the y_range or both m and M must be specified")
   }
 
-  if((!is.null(m) | !is.null(M)) & !is.null(y_range)){
+  if ((!is.null(m) | !is.null(M)) & !is.null(y_range)) {
     stop("Only y_range or m,M can be specified")
   }
 
-  if(!(scale %in% c("higher","lower"))) stop ("scale must be 'higher' or 'lower'")
+  if (!(scale %in% c("higher", "lower")))
+    stop ("scale must be 'higher' or 'lower'")
 
-  if(!(is.null(m) | is.null(M))){
-
-    if(m >= M) stop("m must be less than M")
+  if (!(is.null(m) | is.null(M))) {
+    if (m >= M)
+      stop("m must be less than M")
 
     #if lower and upper bounds are specified, create range based on those
     y_range <- m:M
@@ -92,8 +93,8 @@ lp_ideal_rule_benefit <- function(m = NULL,
   }
 
   #assign which arms are worse or better based on scale
-  if((scale == "lower" & mean_1 <= mean_0) | (scale == "higher" & mean_1 >= mean_0)){
-
+  if ((scale == "lower" &
+       mean_1 <= mean_0) | (scale == "higher" & mean_1 >= mean_0)) {
     mean_better <- mean_1
     var_better <- s2_1
     n_better <- n_1
@@ -103,7 +104,6 @@ lp_ideal_rule_benefit <- function(m = NULL,
     n_worse <- n_0
 
   } else {
-
     mean_better <- mean_0
     var_better <- s2_0
     n_better <- n_0
@@ -125,26 +125,28 @@ lp_ideal_rule_benefit <- function(m = NULL,
   new_mean_better <- temp$new_mean_better
 
 
-  if(conf.upper){
-
-    if(is.null(n_1) | is.null(n_0)){
-
+  if (conf.upper) {
+    if (is.null(n_1) | is.null(n_0)) {
       stop("Values for n_1 and n_0 are required to form confidence intervals.")
 
     }
 
     #get upper confidence bound
-    ci.upper <- helper_lp_ci(y_range = new_y_range,
-                               new_mean_worse,
-                               new_mean_better,
-                               var_worse,
-                               var_better,
-                               n_worse,
-                               n_better,
-                               level = level)
+    ci.upper <- helper_lp_ci(
+      y_range = new_y_range,
+      new_mean_worse,
+      new_mean_better,
+      var_worse,
+      var_better,
+      n_worse,
+      n_better,
+      level = level
+    )
 
 
-  } else {ci.upper <- NULL}
+  } else {
+    ci.upper <- NULL
+  }
 
   #get bound estimate
   bound <- helper_lp_benefit(y_range = new_y_range,
@@ -170,44 +172,56 @@ lp_ideal_rule_benefit <- function(m = NULL,
 #'
 #' @examples #tbc
 helper_lp_benefit <- function(y_range,
-                               mean_worse,
-                               mean_better,
-                               var_worse,
-                               var_better){
+                              mean_worse,
+                              mean_better,
+                              var_worse,
+                              var_better) {
 
+  #get joint distribution of outcome values in both arms
+  support <- expand.grid("y_w" = y_range, "y_b" = y_range)
 
-  support <- expand.grid("y_w" = y_range,"y_b" = y_range)
-
-  #absolute value of treatment effect when less than 0
-  objective.in <- (support$y_w - support$y_b) * (support$y_b < support$y_w)
+  #objective function - absolute value of treatment effect when less than 0
+  objective.in <-
+    (support$y_w - support$y_b) * (support$y_b < support$y_w)
 
   #constraint that all probs add to 1
-  c1 <- rep(1,length(y_range)^2)
+  c1 <- rep(1, length(y_range) ^ 2)
 
-  #constraint on worse mean
+  #left hand side of constraint on worse mean
   c2 <- support$y_w
 
-  #contsraint on better mean
+  #left hand side of constraint on better mean
   c3 <- support$y_b
 
-  #constraint on worse second moment
-  c4 <- support$y_w^2
+  #left hand side of constraint on worse second moment
+  c4 <- support$y_w ^ 2
 
-  #constraint on better second moment
-  c5 <- support$y_b^2
+  #left hand side of constraint on better second moment
+  c5 <- support$y_b ^ 2
 
-  const.mat <- rbind(c1,c2,c3,c4,c5)
+  #matrix of left hand sides of constraints
+  const.mat <- rbind(c1, c2, c3, c4, c5)
 
+  #all constraints are equalities
+  const.dir <- rep("==", 5)
 
-  const.dir <- rep("==",5)
+  #right hand sides of constraints
+  const.rhs <- c(1,
+                 mean_worse,
+                 mean_better,
+                 var_worse + mean_worse ^ 2,
+                 var_better + mean_better ^ 2)
 
-  const.rhs <- c(1,mean_worse,mean_better,
-                 var_worse+mean_worse^2,var_better+mean_better^2)
-
+  #solvea the linear programming problem
+  #   returns the max value of the objective
   return(
-    lpSolve::lp(direction = "max",objective.in = objective.in,
-       const.mat = const.mat, const.dir = const.dir,
-       const.rhs = const.rhs)$objval
+    lpSolve::lp(
+      direction = "max",
+      objective.in = objective.in,
+      const.mat = const.mat,
+      const.dir = const.dir,
+      const.rhs = const.rhs
+    )$objval
   )
 
 }
@@ -225,84 +239,102 @@ helper_lp_benefit <- function(y_range,
 #'
 #' @examples #tbc
 helper_lp_ci <- function(y_range,
-                                        mean_worse,
-                                        mean_better,
-                                        var_worse,
-                                        var_better,
-                                        n_worse,
-                                        n_better,
-                         level){
+                         mean_worse,
+                         mean_better,
+                         var_worse,
+                         var_better,
+                         n_worse,
+                         n_better,
+                         level) {
 
-  q_low = 0.5 - level/2
-  q_high = 0.5 + level/2
+  #get low and high quantiles for given confidence level
+  q_low = 0.5 - level / 2
+  q_high = 0.5 + level / 2
 
-  support <- expand.grid("y_w" = y_range,"y_b" = y_range)
+  #get joint distribution of outcome values in both arms
+  support <- expand.grid("y_w" = y_range, "y_b" = y_range)
 
+  #get bounds on range for use in se bound calculations
   range_low = min(y_range)
-
   range_high = max(y_range)
 
   #se for sample means of worse and better
-  m_w_se <- sqrt(var_worse/n_worse)
-  m_b_se <- sqrt(var_better/n_better)
+  m_w_se <- sqrt(var_worse / n_worse)
+  m_b_se <- sqrt(var_better / n_better)
 
-  #se for sample var + sample mean squared
-  second_moment_w_se <- max((range_high-mean_worse)^2,
-                            (range_low-mean_worse)^2)*var_worse-var_worse^2 +
-    4*mean_worse*max(abs(range_high),abs(range_low))*(var_worse+mean_worse^2)-
-    8*var_worse*mean_worse^2 - 4*mean_worse^4
+  #se for sample var + sample mean squared of worse arm
+  second_moment_w_se <- max((range_high - mean_worse) ^ 2,
+                            (range_low - mean_worse) ^ 2) * var_worse -
+    var_worse ^ 2 +
+    4 * mean_worse * max(abs(range_high), abs(range_low)) * (var_worse +
+                                                               mean_worse ^ 2) -
+    8 * var_worse * mean_worse ^ 2 - 4 * mean_worse ^ 4
 
-  second_moment_w_se <- sqrt(second_moment_w_se/n_worse)
+  second_moment_w_se <- sqrt(second_moment_w_se / n_worse)
 
-  second_moment_B_se <- max((range_high-mean_better)^2,
-                            (range_low-mean_better)^2)*var_better-var_better^2 +
-    4*mean_better*max(abs(range_high),abs(range_low))*(var_better+mean_better^2)-
-    8*var_better*mean_better^2 - 4*mean_better^4
+  #se for sample var + sample mean squared of better arm
+  second_moment_B_se <- max((range_high - mean_better) ^ 2,
+                            (range_low - mean_better) ^ 2) * var_better -
+    var_better ^ 2 +
+    4 * mean_better * max(abs(range_high), abs(range_low)) * (var_better +
+                                                                mean_better ^ 2) -
+    8 * var_better * mean_better ^ 2 - 4 * mean_better ^ 4
 
-  second_moment_B_se <- sqrt(second_moment_B_se/n_better)
+  second_moment_B_se <- sqrt(second_moment_B_se / n_better)
 
-  #absolute value of treatment effect when less than 0
-  objective.in <- (support$y_w - support$y_b) * (support$y_b < support$y_w)
+  #objective function - absolute value of treatment effect when less than 0
+  objective.in <-
+    (support$y_w - support$y_b) * (support$y_b < support$y_w)
 
-  #constraint that all probs add to 1
-  c1 <- rep(1,length(y_range)^2)
+  #left hand side of constraint that all probs add to 1
+  c1 <- rep(1, length(y_range) ^ 2)
 
-  #constraint on worse mean (used twice)
+  #left hand side of constraint on worse mean (used twice)
   c2 <- support$y_w
 
-  #contsraint on better mean (used twice)
+  #left hand side of constraint on better mean (used twice)
   c3 <- support$y_b
 
-  #constraint on worse second moment (used twice)
-  c4 <- support$y_w^2
+  #left hand side of constraint on worse second moment (used twice)
+  c4 <- support$y_w ^ 2
 
-  #constraint on better second moment (used twice)
-  c5 <- support$y_b^2
+  #left hand side of constraint on better second moment (used twice)
+  c5 <- support$y_b ^ 2
 
 
-  const.mat <- rbind(c1,c2,c2,c3,c3,c4,c4,c5,c5)
+  #left hand side of constraints
+  const.mat <- rbind(c1, c2, c2, c3, c3, c4, c4, c5, c5)
 
   #c1 is equality, rest are within lower and upper bounds
-  const.dir <- c("==",rep(c(">=","<="),4))
+  const.dir <- c("==", rep(c(">=", "<="), 4))
 
-  const.rhs <- c(1,
-                 stats::qnorm(c(q_low,q_high),
-                       mean = mean_worse, sd = m_w_se),
-                 stats::qnorm(c(q_low,q_high),
-                       mean = mean_better, sd = m_b_se),
-                 stats::qnorm(c(q_low,q_high),
-                       mean =var_worse+mean_worse^2, sd = second_moment_w_se),
-                 stats::qnorm(c(q_low,q_high),
-                       mean = var_better+mean_better^2, sd = second_moment_B_se))
+  #right hand side of constraints
+  const.rhs <- c(
+    1,
+    stats::qnorm(c(q_low, q_high),
+                 mean = mean_worse, sd = m_w_se),
+    stats::qnorm(c(q_low, q_high),
+                 mean = mean_better, sd = m_b_se),
+    stats::qnorm(
+      c(q_low, q_high),
+      mean = var_worse + mean_worse ^ 2,
+      sd = second_moment_w_se
+    ),
+    stats::qnorm(
+      c(q_low, q_high),
+      mean = var_better + mean_better ^ 2,
+      sd = second_moment_B_se
+    )
+  )
 
-
-  lpSolve::lp(direction = "max",objective.in = objective.in,
-     const.mat = const.mat, const.dir = const.dir,
-     const.rhs = const.rhs)
+  #solvea the linear programming problem
+  #   returns the max value of the objective
+  lpSolve::lp(
+    direction = "max",
+    objective.in = objective.in,
+    const.mat = const.mat,
+    const.dir = const.dir,
+    const.rhs = const.rhs
+  )
 
 }
-
-
-
-
-
