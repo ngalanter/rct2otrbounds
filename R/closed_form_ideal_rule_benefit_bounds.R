b6aef0b07f31c4460de4bc1967d56d7ab8bac150
Nina Galanter <ninagalanter@gmail.com>
2022-07-18 23:18:39 -0700

    Update closed_form_ideal_rule_benefit_bounds.R

--- R/closed_form_ideal_rule_benefit_bounds.R
+++ R/closed_form_ideal_rule_benefit_bounds.R
@@ -38,6 +38,7 @@ closed_form_ideal_rule_benefit <- function(s2_1,
       warning("No confidence interval can be provided for unbounded outcomes")
   }
 
+  #get upper bound on treatment effect heterogeneity
   teh_bound <- treatment_effect_heterogeneity_bound(s2_1 = s2_1,
                                                     s2_0 = s2_0,
                                                     n_1 = n_1,
@@ -49,26 +50,34 @@ closed_form_ideal_rule_benefit <- function(s2_1,
                                                     bounded_outcome = bounded_outcome,
                                                     conf.int = FALSE)$estimates[2]
 
+  #get point estimate of upper bound on ideal treatment rule benefit
   estimate <- 0.5 * sqrt( teh_bound + (mean_1 - mean_0)^2 )
 
-  if(conf.upper){
+  if(conf.upper & bounded_outcome){
 
+    #total sample size
     n <- n_1 + n_0
 
+    #proportion of sample size in treatment arm
     r_1 <- n_1/n
 
+    #proportion of sample size in control arm
     r_0 <- n_0/n
 
+    #sample standard deviation ratio
     nu_hat <- sqrt(s2_1/s2_0)
 
+    #sample treatment effect
     mean_delta <- mean_1 - mean_0
 
+    #bounds on variance of squared sd estimates in each arm
     max_sqdev_1 <- max((M-mean_1)^2,(m-mean_1)^2)
-
     max_sqdev_0 <- max((M-mean_0)^2,(m-mean_0)^2)
 
+    #point estimate based on general bound
     point <- 1/2*sqrt(s2_0*(nu_hat+1)^2 +mean_delta^2)
 
+    #bound on the variance of the point estimate
     var <- (16*(s2_0*(nu_hat+1)^2+mean_delta^2))^(-1)*
       (
         (max_sqdev_1*s2_1-s2_1^2)*(nu_hat^(-1)+ 1)^2/r_1 +
@@ -78,6 +87,7 @@ closed_form_ideal_rule_benefit <- function(s2_1,
           (max(abs(M),abs(m))*(s2_0+mean_0^2)+3*mean_0*s2_0+mean_0^3)*2*mean_delta*(1+nu_hat)/r_0
       )
 
+    #upper confidence bound at the specified level
     ci.upper <- stats::qnorm(level, mean = point, sd = sqrt(var/n))
 
   } else {ci.upper <- NULL}
