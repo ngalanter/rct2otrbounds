5fed61c85caa8c3247169a538342009896ba3756
Nina Galanter <ninagalanter@gmail.com>
2022-07-18 21:01:25 -0700

    creating teh bound and lp benefit bound functions

---
+++ R/ideal_rule_benefit_bounds.R
@@ -0,0 +1,262 @@
+#' Linear Programming Bound on the Ideal Rule Benefit
+#'
+#' @param scale Scale should be "higher" (default) if higher outcomes are beneficial,
+#'     and "lower" otherwise
+#' @param conf.upper logical; whether a upper confidence bound should be provided
+#' @inheritParams teh_closed_form_bounded
+#'
+#'
+#' @returns
+#' #' A list with two elements:
+#'
+#' * `estimate` the estimated benefit of the ideal treatment rule over the best uniform treatment
+#' * `conf.upper` The `level` upper confidence bound if `conf.upper` is TRUE, `NULL` otherwise
+#'
+#' @examples #tbc
+#'
+#' @export
+lp_ideal_rule_benefit <- function(m = NULL,
+                                          M = NULL,
+                                          y_range = NULL,
+                                              s2_1,
+                                              s2_0,
+                                              mean_1,
+                                              mean_0,
+                                              n_1=NULL,
+                                              n_0=NULL,
+                                              scale = "higher",
+                                              conf.upper = FALSE,
+                                              level = 0.95)
+{
+
+  if((is.null(m) | is.null(M)) & is.null(y_range)){
+    stop("Either the y_range or both m and M must be specified")
+  }
+
+  if((!is.null(m) | !is.null(M)) & !is.null(y_range)){
+    stop("Only y_range or m,M can be specified")
+  }
+
+  if(!(scale %in% c("higher","lower"))) stop ("scale must be 'higher' or 'lower'")
+
+  if(!(is.null(m) | is.null(M))){
+
+    if(m >= M) stop("m must be less than M")
+
+    y_range <- m:M
+
+  }
+
+  if((scale == "lower" & mean_1 <= mean_0) | (scale == "higher" & mean_1 >= mean_0)){
+
+    mean_better <- mean_1
+    var_better <- s2_1
+    n_better <- n_1
+
+    mean_worse <- mean_0
+    var_worse <- s2_0
+    n_worse <- n_0
+
+  } else {
+
+    mean_better <- mean_0
+    var_better <- s2_0
+    n_better <- n_0
+
+    mean_worse <- mean_1
+    var_worse <- s2_1
+    n_worse <- n_1
+
+  }
+
+  #flip outcome scale if lower outcomes are better
+  if(scale == "lower"){
+
+    new_y_range <- -1*y_range + max(y_range)
+    new_mean_worse <- max(y_range) - mean_worse
+    new_mean_better <- max(y_range) - mean_better
+
+  } else{
+
+    new_y_range <- y_range - min(y_range)
+    new_mean_worse <- mean_worse - min(y_range)
+    new_mean_better <- mean_better - min(y_range)
+
+  }
+
+  if(conf.upper){
+
+    if(is.null(n_1) | is.null(n_0)){
+
+      stop("Values for n_1 and n_0 are required to form confidence intervals.")
+
+    }
+
+    conf.upper <- helper_lp_ci(y_range = new_m:new_M,
+                               new_mean_worse,
+                               new_mean_better,
+                               var_worse,
+                               var_better,
+                               n_worse,
+                               n_better,
+                               level = level)
+
+
+  } else {conf.upper <- NULL}
+
+  bound <- helper_lp_benefit(y_range = new_m:new_M,
+                             new_mean_worse,
+                             new_mean_better,
+                             var_worse,
+                             var_better)
+
+  return(list(estimate = bound, conf.upper = conf.upper))
+
+
+}
+
+#' Helper Function to Calculate Linear Program Bound on Ideal Rule Benefit
+#'
+#' @param y_range The range of possible outcomes, should be nonnegative
+#' @param mean_worse The estimated mean of the worse treatment
+#' @param mean_better The estimated mean of the better treatment
+#' @param var_worse The estimated variance of the worse treatment
+#' @param var_better The estimated variance of the better treatment
+#'
+#' @return the estimated benefit of the ideal treatment rule over the best uniform treatment
+#'
+#' @examples #tbc
+helper_lp_benefit <- function(y_range,
+                               mean_worse,
+                               mean_better,
+                               var_worse,
+                               var_better){
+
+
+  support <- expand.grid("y_w" = y_range,"y_b" = y_range)
+
+  #absolute value of treatment effect when less than 0
+  objective.in <- (support$y_w - support$y_b) * (support$y_b < support$y_w)
+
+  #constraint that all probs add to 1
+  c1 <- rep(1,length(y_range)^2)
+
+  #constraint on worse mean
+  c2 <- support$y_w
+
+  #contsraint on better mean
+  c3 <- support$y_b
+
+  #constraint on worse second moment
+  c4 <- support$y_w^2
+
+  #constraint on better second moment
+  c5 <- support$y_b^2
+
+  const.mat <- rbind(c1,c2,c3,c4,c5)
+
+
+  const.dir <- rep("==",5)
+
+  const.rhs <- c(1,mean_worse,mean_better,
+                 var_worse+mean_worse^2,var_better+mean_better^2)
+
+  return(
+    lpSolve::lp(direction = "max",objective.in = objective.in,
+       const.mat = const.mat, const.dir = const.dir,
+       const.rhs = const.rhs)$objval
+  )
+
+}
+
+#' Helper Function to Calculate Linear Program Upper Confidence Bound on Ideal Rule Benefit
+#'
+#' @param n_worse Sample size of worse treatment
+#' @param n_better Sample size of better treatment
+#' @param level confidence level between 0 and 1 for confidence interval;
+#'     default is 0.95
+#' @inheritParams helper_lp_benefit
+#'
+#' @return
+#' @export
+#'
+#' @examples #tbc
+helper_lp_ci <- function(y_range,
+                                        mean_worse,
+                                        mean_better,
+                                        var_worse,
+                                        var_better,
+                                        n_worse,
+                                        n_better,
+                         level){
+
+  q_low = 0.5 - level/2
+  q_high = 0.5 + level/2
+
+  support <- expand.grid("y_w" = y_range,"y_b" = y_range)
+
+  range_low = min(y_range)
+
+  range_high = max(y_range)
+
+  #se for sample means of worse and better
+  m_w_se <- sqrt(var_worse/n_worse)
+  m_b_se <- sqrt(var_better/n_better)
+
+  #se for sample var + sample mean squared
+  second_moment_w_se <- max((range_high-mean_worse)^2,
+                            (range_low-mean_worse)^2)*var_worse-var_worse^2 +
+    4*mean_worse*max(abs(range_high),abs(range_low))*(var_worse+mean_worse^2)-
+    8*var_worse*mean_worse^2 - 4*mean_worse^4
+
+  second_moment_w_se <- sqrt(second_moment_w_se/n_worse)
+
+  second_moment_B_se <- max((range_high-mean_better)^2,
+                            (range_low-mean_better)^2)*var_better-var_better^2 +
+    4*mean_better*max(abs(range_high),abs(range_low))*(var_better+mean_better^2)-
+    8*var_better*mean_better^2 - 4*mean_better^4
+
+  second_moment_B_se <- sqrt(second_moment_B_se/n_better)
+
+  #absolute value of treatment effect when less than 0
+  objective.in <- (support$y_w - support$y_b) * (support$y_b < support$y_w)
+
+  #constraint that all probs add to 1
+  c1 <- rep(1,length(y_range)^2)
+
+  #constraint on worse mean (used twice)
+  c2 <- support$y_w
+
+  #contsraint on better mean (used twice)
+  c3 <- support$y_b
+
+  #constraint on worse second moment (used twice)
+  c4 <- support$y_w^2
+
+  #constraint on better second moment (used twice)
+  c5 <- support$y_b^2
+
+
+  const.mat <- rbind(c1,c2,c2,c3,c3,c4,c4,c5,c5)
+
+  #c1 is equality, rest are within lower and upper bounds
+  const.dir <- c("==",rep(c(">=","<="),4))
+
+  const.rhs <- c(1,
+                 stats::qnorm(c(q_low,q_high),
+                       mean = mean_worse, sd = m_w_se),
+                 stats::qnorm(c(q_low,q_high),
+                       mean = mean_better, sd = m_b_se),
+                 stats::qnorm(c(q_low,q_high),
+                       mean =var_worse+mean_worse^2, sd = second_moment_w_se),
+                 stats::qnorm(c(q_low,q_high),
+                       mean = var_better+mean_better^2, sd = second_moment_B_se))
+
+
+  lpSolve::lp(direction = "max",objective.in = objective.in,
+     const.mat = const.mat, const.dir = const.dir,
+     const.rhs = const.rhs)
+
+}
+
+
---
+++ R/teh_bounds.R
@@ -0,0 +1,155 @@
+
+#' Closed Form Treatment Effect Heterogeneity Bound for Bounded Outcomes
+#'
+#' @param m,M two real numbers defining the outcome range with `m`<`M`
+#' @param s2_1 estimated outcome variance of the treatment group
+#' @param s2_0 estimated outcome variance of the control group
+#' @param n_1 sample size of the treatment group
+#' @param n_0 sample size of the control group
+#' @param mean_1 mean outcome of the treatment group
+#' @param mean_0 mean outcome of the control group
+#' @param conf.int whether a confidence interval is generated
+#' @param level confidence level between 0 and 1 for confidence interval
+#'     default is 0.95
+#'
+#' @returns
+#' A list with two elements:
+#'
+#' * `estimates` a two element vector of the estimated lower and upper bounds on
+#'     treatment effect heterogeneity
+#' * `conf.int` a two element vector of the `level` confidence interval around the bounds,
+#'     for bounded outcomes when the `conf.int` argument is set to TRUE
+#' @examples #tbd
+#'
+teh_closed_form_bounded <- function(m, M, s2_1, s2_0, n_1=NULL, n_0=NULL,
+                                    mean_1=NULL, mean_0=NULL,
+                                    conf.int = FALSE, level = 0.95)
+{
+
+
+  #calculating the upper and lower bounds on treatment effect heterogeneity for bounded outcomes
+  point_low <- s2_1+s2_0-2*min(sqrt(s2_1)*sqrt(s2_0),
+                               (M-mean_1)*(M-mean_0),
+                               (mean_1-m)*(mean_0-m))
+
+  point_high <- s2_1+s2_0+2*min(sqrt(s2_1)*sqrt(s2_0),
+                                (M-mean_1)*(mean_0-m),
+                                (mean_1-m)*(M-mean_0))
+
+
+  if(conf.int){
+
+    if(any(sapply(list(n_1,n_0,mean_1,mean_0), is.null))){
+
+      stop("Values for n_1,n_0,mean_1,and mean_0 are required to form confidence intervals.")
+    }
+
+    q_low = 0.5 - level/2
+    q_high = 0.5 + level/2
+
+    #total sample size
+    n <- n_1 + n_0
+
+    #proportion of sample size in each trial arm
+    r_1 <- n_1/n
+    r_0 <- n_0/n
+
+    #sample variance ratio of treatment to control arm
+    nu_hat <- sqrt(s2_1/s2_0)
+
+    #upper bound on the variance of general lower bound on teh
+    var_low <- ( max((m-mean_1)^2,(M-mean_1)^2)*s2_1 - s2_1^2 ) * (1/nu_hat - 1)^2 / r_1 +
+      ( max((m-mean_0)^2,(M-mean_0)^2)*s2_0 - s2_0^2 ) * (nu_hat - 1)^2 / r_0
+
+    #upper bound on the variance of general upper bound on teh
+    var_high <- ( max((m-mean_1)^2,(M-mean_1)^2)*s2_1 - s2_1^2 ) * (1/nu_hat + 1)^2 / r_1 +
+      ( max((m-mean_0)^2,(M-mean_0)^2)*s2_0 - s2_0^2 ) * (nu_hat + 1)^2 / r_0
+
+    #lower confidence interval limit for the bounds
+    ci_low <- stats::qnorm(q_low, mean = point_low, sd = sqrt(var_low/n))
+
+    #upper confidence interval limit for the bounds
+    ci_high <- stats::qnorm(q_high, mean = point_high, sd = sqrt(var_high/n))
+  } else {
+    ci_low = NULL
+    ci_high = NULL
+  }
+  return(list(estimates = c(point_low,point_high),conf.int= c(ci_low,ci_high)))
+
+
+
+}
+
+#' Closed Form Treatment Effect Heterogeneity Bound for Unbounded Outcomes
+#'
+#' @param s2_1 estimated outcome variance of the treatment group
+#' @param s2_0 estimated outcome variance of the control group
+#'
+#' @returns
+#' A a vector with the lower bound on treatment effect variance followed by the upper bound
+#'
+#' @examples #tbd
+#'
+teh_closed_form_unbounded <- function(s2_1,s2_0){
+
+  #sample variance ratio of treatment to control arm
+  nu_hat <- sqrt(s2_1/s2_0)
+
+  #calculating the upper and lower bounds on treatment effect heterogeneity for unbounded outcomes
+  low <- s2_0*(nu_hat-1)^2
+
+  high <- s2_0*(nu_hat+1)^2
+
+
+  return(c(low,high))
+
+}
+
+#' General Closed Form Treatment Effect Heterogeneity Bound
+#'
+#' @param bounded_outcome logical; `TRUE` if the outcome is bounded and `FALSE` otherwise,
+#'     default is `FALSE`
+#' @inheritParams teh_closed_form_bounded
+#'
+#' @returns
+#' A list with two elements:
+#'
+#' * `estimates` a two element vector of the estimated lower and upper bounds on
+#'     treatment effect heterogeneity
+#' * `conf.int` a two element vector of the `level` confidence interval around the bounds,
+#'     for bounded outcomes when the `conf.int` argument is set to TRUE
+#'
+#' @examples #tbd
+#'
+#' @export
+treatment_effect_heterogeneity_bound <- function(s2_1,s2_0,
+                                                 n_1 = NULL,n_0 = NULL,
+                                                 mean_1 = NULL,mean_0 = NULL,
+                                                 m = NULL,M = NULL,level = 0.95,
+                                                 bounded_outcome = F, conf.int = F)
+  {
+
+
+
+  if(bounded_outcome){
+
+    if(bounded_outcome & (is.null(m) | is.null(M))) {
+      stop("For bounded outcomes, bounds must be given by the m and M arguments")
+    }
+
+    if(bounded_outcome & (m >= M)) {
+      stop("For bounded outcomes, m must be less than M")
+    }
+
+    return(teh_closed_form_bounded(m=m,M=M,s2_1=s2_1,s2_0=s2_0,
+                                   n_1=n_1,n_0=n_0,mean_1=mean_1,mean_0=mean_0,
+                                   conf.int = conf.int, level = level))
+  } else{
+
+    if(conf.int) warning("No confidence interval can be provided for unbounded outcomes")
+
+    return(list(estimates = teh_closed_form_unbounded(s2_1,s2_0),
+                conf.int = c(NULL,NULL)))
+  }
+
+}
